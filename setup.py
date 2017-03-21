105b81cb1a080adf15651bdd9727c9879743b9bb
Christoph Rosemann <christoph.rosemann@desy.de>
2017-03-21 11:22:08 +0100

    prepare the python packaging  - specify the requirements: [numpy, h5py, six, fabio]   --> why does this trigger a rebuild for h5py??

--- setup.py
+++ setup.py
@@ -1,31 +1,68 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from sphinx.setup_command import BuildDoc
 
-name='PSIO'
-version='0.1'
-release='dev'
+from codecs import open
+from os import path
+
+with open(path.join('.', 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='PSIO',
-    version='0.1dev',
+    name='psio',
+
+    version='0.1.0dev',
+
+    description='Module to facilitate access for photon science data in different formats.', 
+    long_description=long_description,
+
+    url='https://github.com/syncope/psio',
+
     author='Ch.Rosemann',
     author_email='christoph.rosemann@desy.de',
-    packages=['psio','psio.test',],
-    scripts=['bin/counting_bad_pixels.py',
-        'bin/simple_file_viewer.py',
-        'bin/simpleViewerG.ui',
-        'bin/simpleViewerGUI.py',
-        'bin/stupid_gisaxs-p03.py'],
-    description='Module to facilitate access for photon science data in different formats.', 
-    license='LICENSE',
-    long_description=open('README').read(),
-    data_files=[('.', ['COPYRIGHT','LICENCSE']), ],
-    cmdclass={'build_sphinx': BuildDoc,},
-    command_options={
-        'build_sphinx': {
-            'project': ('setup.py', name),
-            'version': ('setup.py', version),
-            'release': ('setup.py', release)}},
+    
+    license='GPLv2',
+    
+    classifiers = [
+        'Development status :: 3 - Alpha',
+        
+        'Intended audience :: photon scientists',
+        'Topic :: File IO',
+        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='photon science file input output',
+    
+    packages=find_packages(),
+
+    install_requires=['numpy', 'six', 'h5py','fabio'], 
+    
+    extras_require={
+        'test': ['files']
+        },
+    #~ entry_points={
+        #~ 'console_scripts': [
+            #~ '',
+        #~ ],
+
+    #~ scripts=['bin/counting_bad_pixels.py',
+        #~ 'bin/simple_file_viewer.py',
+        #~ 'bin/simpleViewerG.ui',
+        #~ 'bin/simpleViewerGUI.py',
+        #~ 'bin/stupid_gisaxs-p03.py'],
+    
+    data_files=[('.', ['COPYRIGHT','LICENSE']), ],
+
+    #~ cmdclass={'build_sphinx': BuildDoc,},
+    #~ command_options={
+        #~ 'build_sphinx': {
+            #~ 'project': ('setup.py', name),
+            #~ 'version': ('setup.py', version),
+            #~ 'release': ('setup.py', release)}},
 )
 
