9a3b778ecfbf73edce1c5582e5ee65619c83b747
Christoph Rosemann <christoph.rosemann@desy.de>
2017-07-06 18:26:26 +0200

    re-work of the output modules + added new hdf5 only output, tailored to use only h5py if pni libs are missing + introduced common base class for this
     group implementation works, including "fake" nexus output formatting by using group attributes for naming classes
    
    ! still some trouble in sorting out the details of using h5py api
     especially concerning the datasets, different logic; concerning the automatic "growth" of datasets for addition of data
    
    ? unclear status on attribute/comment addition (seems to work, but still untested)

--- psio/hdf5Output.py
+++ psio/hdf5Output.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-17  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# Copyright (C) 2017  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
 # email contact: christoph.rosemann@desy.de
 #
 # This program is free software; you can redistribute it and/or
@@ -17,21 +17,23 @@
 # Boston, MA  02110-1301, USA.
 
 
-'''Implementation for handling files based on NeXus file format.
-For more information on NeXus visit www.nexusformat.org'''
+'''Implementation for handling files based on HDF without pni library.
+Structure is the same, as well as the functionality.'''
 
 import outputBase
-import pni.io.nx.h5 as nexus
+import h5py
+import numpy as np
 
 
-class NexusOutput(outputBase.OutputBase):
+class HDF5Output(outputBase.OutputBase):
 
-    def __init__(self, filename, recreate):
-        self._file = nexus.create_file(filename, overwrite=recreate)
-        self._root = self._file.root()
-        temp = self._root.create_group("entry:NXentry")
-        self._defaultGroup = temp.create_group("data:NXdata")
-        self._fields = {}
+    def __init__(self, filename, mode='w'):
+        self._file = h5py.File(filename,mode=mode)
+        self._entry = self._file.create_group("entry")
+        self._entry.attrs["NX_class"] = "NXentry"
+        self._defaultGroup = self._entry.create_group("data")
+        self._defaultGroup.attrs["NX_class"] = "NXdata"
+        self._datasets = {} # called fields in nexus
 
     def write(self):
         self._file.flush()
@@ -43,39 +45,53 @@ class NexusOutput(outputBase.OutputBase):
     def addField(self, name, dimension, dtype="float32"):
         '''Adding a field to hold data at the default location.'''
         try:
-            chunk = (1,) + dimension[1:]
+            correctedDimension = dimension[1:]
         except TypeError:
-            chunk = (1,) + (dimension, )
+            correctedDimension = (dimension, )
+        chunk = (1,) + correctedDimension
         try:
-            self._fields[name] = self._defaultGroup.create_field(
-                name, dtype, dimension, chunk)
+            if name in self._datasets:
+                raise ValueError("Trying to create a field/dataset that already exists.")
+            else:
+                print(" CREATE DS with chunks = " + str(chunk))
+                self._datasets[name] = self._defaultGroup.create_dataset(
+                    name=name, dtype=dtype, shape=chunk, chunks=chunk, maxshape=(None, correctedDimension))
         except AttributeError:
             print(
-                "Could not create the field for multiple data.")
+                "Could not create a field to hold images/data.")
 
     def addSingleImageField(self, name, dimension, dtype="float32"):
+        pass
         '''Adding a field to hold a single image.'''
         try:
             self._fields[name] = self._defaultGroup.create_field(
                 name, dtype, dimension)
         except AttributeError:
             print(
-                "Could not create the field for single images.")
+                "Could not create a single image field element.")
 
     def addDataToField(self, name, data):
         try:
-            self._fields[name].grow(0, 1)
-            self._fields[name][-1, ...] = data
+            initialshape = self._datasets[name].shape
+            print("shape bf: " + repr(initialshape))
+            newlength = initialshape[0]+1
+            temptpl = (newlength, ) + initialshape[1:]
+            print(" trying hard to create: " + repr(temptpl))
+            self._datasets[name].resize(temptpl)
+            print(" shape af: " + repr(self._datasets[name].shape))
+            self._datasets[name][-1, ...] = data
         except KeyError:
             print("Field " + name + " doesn't exist.")
 
     def addSingleImageToField(self, name, data):
+        pass
         try:
             self._fields[name].write(data)
         except KeyError:
             print("Field " + name + " doesn't exist.")
 
     def addAttributeToField(self, fieldname, title, value):
+        pass
         try:
             self._fields[fieldname].attributes.create(
                 title, "float64")[...] = value
@@ -83,6 +99,7 @@ class NexusOutput(outputBase.OutputBase):
             print("Field " + fieldname + " doesn't exist.")
 
     def addCommentToField(self, fieldname, title, comment):
+        pass
         try:
             self._fields[fieldname].attributes.create(
                 title, "str")[...] = comment
@@ -92,11 +109,11 @@ class NexusOutput(outputBase.OutputBase):
 
 if(__name__ == "__main__"):
     print("It's working.")
-    no = NexusOutput("test.ndf", recreate=True)
+    no = HDF5Output("test.h5", mode='w')
     no.addField("test", (0, 2, 2))
     no.addField("test2", (2, 2))
-    no.addCommentToField("test", "com2", "we dont need no")
-    no.addAttributeToField("test2", "answer", 42.)
+    #~ no.addCommentToField("test", "com2", "we dont need no")
+    #~ no.addAttributeToField("test2", "answer", 42.)
 
     import numpy as np
     d = np.array([1, 2, 3, 4])
