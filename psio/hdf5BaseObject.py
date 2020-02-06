eab42d4a6733d58dc85a0c82a2582088263e68f7
Christoph Rosemann <christoph.rosemann@desy.de>
2020-02-06 08:40:14 +0100

    + save intermediate step

---
+++ psio/hdf5BaseObject.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2020  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# email contact: christoph.rosemann@desy.de
+#
+# psio :: photon science input ouput is a library to facilitate the 
+# access to file based data by offering unified services to different
+# data formats
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation in  version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
+
+
+
+class HD5FBaseObject():
+
+    def __init__(self):
+        self._unit = ''
+        self._name = ''
+        self._value = ''
+        self._path = ''
+
+    def unit(self):
+        return self._unit
+
+    def name(self):
+        return self._name
+
+    def value(self):
+        return self._value
+
+    def path(self):
+        return self._path
---
+++ psio/lambdaNexusReader.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2020  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# email contact: christoph.rosemann@desy.de
+#
+# psio :: photon science input ouput is a library to facilitate the 
+# access to file based data by offering unified services to different
+# data formats
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation in  version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
+
+
+'''The Lambda NeXus Reader facilitates the access to the hdf5 format 
+   files written by the lambda detector family.'''
+
+
+from . import dataHandler
+
+
+class LambdaNexusReader():
+
+    def __init__(self):
+        '''The constructor.'''
+
+    def getTranslation(self):
+        '''Returns the translation.'''
+        pass
+
+    def getPixelSize(self):
+        '''Returns the pixel size.'''
+        pass
+
+    def getFlatfield(self):
+        '''Returns the flat field.'''
+        pass
+
+    def isFlatfieldApplied(self):
+        '''Returns wether the flatfield has been applied.'''
+        pass
+
+    def getPixelMask(self):
+        '''Returns the pixel mask.'''
+        pass
+
+    def isPixelMaskApplied(self):
+        '''Returns wether the pixel mask has been applied.'''
+        pass
+
+    def getCountTime(self):
+        '''Returns the integration time.'''
+        pass
