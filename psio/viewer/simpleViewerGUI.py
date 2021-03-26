8dbae61f9566c234d6522589f34b8baebed6c396
Christoph Rosemann <christoph.rosemann@desy.de>
2021-03-26 10:54:02 +0100

    + add late parts

---
+++ psio/viewer/simpleViewerGUI.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'simpleViewerG.ui'
+#
+# Created: Tue Jul 25 10:40:15 2017
+#      by: PyQt4 UI code generator 4.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(928, 453)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.displaypart = ImageView(self.centralwidget)
+        self.displaypart.setObjectName(_fromUtf8("displaypart"))
+        self.gridLayout_2.addWidget(self.displaypart, 0, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 928, 19))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.uvw = QtGui.QMenu(self.menubar)
+        self.uvw.setObjectName(_fromUtf8("uvw"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.uvw.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.uvw.setTitle(_translate("MainWindow", "&File", None))
+
+from pyqtgraph import ImageView
---
+++ psio/viewer/simple_file_viewer.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt4 import QtCore, QtGui
+
+from simpleViewerGUI import Ui_MainWindow
+
+import psio
+
+import json
+
+class MyForm(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        openfile = QtGui.QAction(QtGui.QIcon('open.png'), 'Open', self)
+        openfile.setShortcut('Ctrl+O')
+        openfile.setStatusTip('Open new File')
+        openfile.triggered.connect(self.showDialog)
+
+        self.ui.uvw.addAction(openfile)       
+ 
+    def showDialog(self):
+
+        fname = str(QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
+                '.'))
+                
+        if (fname == ''):
+            return
+        io = psio.dataHandler.DataHandler()
+        io.create_reader(fname)
+        #self.head = io.getHeader()
+        #info = json.dumps(self.head)
+        #self.ui.xyz.setText(info)
+
+        self.nda = None
+        for d in io:
+            self.nda = d
+            self.ui.displaypart.setImage(self.nda)
+    
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyForm()
+    myapp.show()
+    sys.exit(app.exec_())
